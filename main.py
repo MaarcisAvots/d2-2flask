5a82425a45cea705557d585f8510ef8ef9456635
MaarcisAvots <56530581+MaarcisAvots@users.noreply.github.com>
2020-02-04 12:07:53 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+@app.route("/home")
+def home():
+  return render_template('home.html')
+
+@app.route("/about")
+def getAbout():
+  return render_template('about.html')
+
+@app.route("/contact")
+def contact():
+  return render_template('contact.html', phone = 87654321)
+
+@app.route('/params')
+def params():
+  return request.args
+
+@app.route('/post_req', methods = ['POST'])
+def post_req():
+  return request.args
+
+
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", threaded=True, port=5000, debug=True)
